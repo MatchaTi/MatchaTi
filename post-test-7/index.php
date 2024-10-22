f5cbbda41a05e66fb23ec87ca3e85b4ae6d8da16
MatchaTi <tehmatcha28@gmail.com>
2024-10-22 15:16:00 +0800

    post test 7

---
+++ post-test-7/index.php
@@ -0,0 +1,15 @@
+<?php
+    echo "Hello World!";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat expedita dolorum, quisquam aliquid, odio a recusandae quas veniam, consectetur cum placeat sit asperiores voluptatem! Placeat odit nulla ad voluptatum saepe?
+</body>
+</html>
